74873ead1e1c667866137c071e642aa129c47dbc
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-21 18:24:03 -0300

    Sbundo aplicação

---
+++ script.js
@@ -0,0 +1,22 @@
+function getDadosEnderecoPorCEP(cep) {
+    console.log(cep);
+    let url = 'https://viacep.com.br/ws/' + cep + '/json';
+    console.log(url);
+
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', url);
+    xmlHttp.onreadystatechange = () => {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            let dadosJSONText = xmlHttp.responseText;
+            let dadosJSONObj = JSON.parse(dadosJSONText);
+
+            document.getElementById('endereco').value = dadosJSONObj.logradouro
+            document.getElementById('bairro').value = dadosJSONObj.bairro
+            document.getElementById('cidade').value = dadosJSONObj.localidade
+            document.getElementById('uf').value = dadosJSONObj.uf
+            
+            console.log(dadosJSONObj)
+        }
+    }
+    xmlHttp.send();
+}
